d674c45a7fb9894810c3f43102f0120c0b77a518
ngngardner <ngngardner@gmail.com>
2021-09-28 11:17:52 -0700

    add bashrc

--- flake.nix
+++ flake.nix
@@ -4,13 +4,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-21.05";
     flake-utils = { url = "github:numtide/flake-utils"; };
+    flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
     pylatex_src = {
       url = "github:JelteF/PyLaTeX";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, pylatex_src }:
+  outputs = { self, nixpkgs, flake-utils, flake-compat, pylatex_src }:
     {
       overlay = final: prev: {
         pylatex = final.callPackage ./python/pylatex.nix { inherit pylatex_src; };
@@ -27,7 +28,12 @@
           };
         in
         {
-          devShell = import ./shell.nix { inherit pkgs;};
+          devShell = pkgs.mkShell {
+            packages = [
+              pkgs.python39
+              pkgs.pylatex
+            ];
+          };
         })
     );
 }
--- shell.nix
+++ shell.nix
@@ -1,10 +1,11 @@
-{ pkgs }:
-pkgs.mkShell {
-  packages = [
-    pkgs.python39
-    pkgs.pylatex
-
-    # nix dev
-    pkgs.nixpkgs-fmt
-  ];
-}
+(import
+  (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+      fetchTarball {
+        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+        sha256 = lock.nodes.flake-compat.locked.narHash;
+      }
+  )
+).shellNix
