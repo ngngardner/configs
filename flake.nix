84b70c2a4e7e3c91489f01d22cfe7b9e243e30d2
Noah Gardner <ngngardner@gmail.com>
2021-10-11 21:58:09 -0400

    pin nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,22 +2,24 @@
   description = "Nix configs";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-21.05";
-    flake-utils = { url = "github:numtide/flake-utils"; };
-    flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/4df9f1a69b1b5d9d79d7d407c4998887c54cc2f4";
+    };
+    utils = { url = "github:numtide/flake-utils"; };
+    compat = { url = "github:edolstra/flake-compat"; flake = false; };
     # python
     pylatex-flake = { url = "github:JelteF/PyLaTeX"; flake = false; };
     pyjulia-flake = { url = "github:JuliaPy/pyjulia"; flake = false; };
   };
 
-  outputs = { self, nixpkgs, flake-utils, flake-compat, pylatex-flake, pyjulia-flake }:
+  outputs = { self, nixpkgs, utils, compat, pylatex-flake, pyjulia-flake }:
     {
       overlay = final: prev: {
         pylatex = final.callPackage ./python/pylatex.nix { inherit pylatex-flake; };
         pyjulia = final.callPackage ./python/pyjulia.nix { inherit pyjulia-flake; };
       };
     } //
-    (flake-utils.lib.eachDefaultSystem
+    (utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = import nixpkgs {
